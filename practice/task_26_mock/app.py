e4a69b1414de0dbf3ada3ca9ce07b65b24be6d2b
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2024-05-19 21:37:39 +0200

    mock data

--- practice/task_26_mock/app.py
+++ practice/task_26_mock/app.py
@@ -1,8 +1,26 @@
-from practice.task_26_mock.external_api import fetch_data_from_api, process_data
+import requests
+from fastapi import FastAPI, Depends
+
+app = FastAPI()
+
+EXTERNAL_API_URL = "https://catfact.ninja/fact"
+
+def fetch_data_from_api():
+    response = requests.get("EXTERNAL_API_URL")
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+def process_data(data):
+    new_data = {}
+    for key, value in data.items():
+        new_data[key.upper()] = value.upper()
+    return new_data
 
 def get_and_process_data():
-    data = fetch_data_from_api()
+    data: dict = fetch_data_from_api()
     if data:
         return process_data(data)
     else:
-        return None
+        return {"error": "Failed to fetch data from the external API"}
--- tests/test_task_26_mock.py
+++ tests/test_task_26_mock.py
@@ -1,20 +1,24 @@
 import unittest
-from unittest.mock import patch, MagicMock
-from practice.task_26_mock.app import get_and_process_data
+from fastapi.testclient import TestClient
+from unittest.mock import patch
+from practice.task_26_mock.app import app, fetch_data_from_api, process_data
+
+client = TestClient(app)
 
 class TestApp(unittest.TestCase):
 
-    @patch("app.fetch_data_from_api")
+    @patch("app.fetch_data_from_api")  # patch - used to replace functions with fake objects
     @patch("app.process_data")
-    def test_get_and_process_data(self, mock_process_data: MagicMock, mock_fetch_data:MagicMock):
+    def test_get_and_process_data(self, mock_process_data, mock_fetch_data):
         mock_response = {"key": "value"}
         mock_fetch_data.return_value = mock_response
 
         mock_process_data = {"KEY": "VALUE"}
         mock_process_data.return_value = mock_process_data
 
-        result = get_and_process_data()
+        response = client.get("/data/")
 
         mock_fetch_data.assert_called_once()
         mock_process_data.assert_called_once_with(mock_response)
-        self.assertEqual(result, mock_process_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), mock_process_data)
